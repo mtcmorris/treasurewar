6f88f62957de42860325156e69ed3fe63c825f0d
Ben Hoskings <ben@hoskings.net>
2012-11-16 21:52:36 +1100

    Fix some regexes so the server is restarted properly.

--- babushka-deps/deploy.rb
+++ babushka-deps/deploy.rb
@@ -4,13 +4,13 @@ end
 
 dep 'server restarted', :template => 'task' do
   run {
-    output = shell?('ps ux | grep -v grep | grep "coffee server.coffee"')
+    output = shell?('ps ux | grep -v grep | grep "node .* server.coffee"')
 
     if output.nil?
       log "`coffee server.coffee` isn't running."
       true
     else
-      pid = output.scan(/^\w+\s+(\d+)\s+/).flatten.first
+      pid = output.scan(/^\S+\s+(\d+)\s+/).flatten.first
       log_shell "Sending SIGTERM to #{pid}", "kill -s TERM #{pid}"
     end
   }
