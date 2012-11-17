278468c2768263dad1bd31a2ed62d4bea710f627
Ben Hoskings <ben@hoskings.net>
2012-11-17 22:05:15 +1100

    Restart the browser on deploy.

--- babushka-deps/deploy.rb
+++ babushka-deps/deploy.rb
@@ -1,5 +1,6 @@
 dep 'after deploy', :old_id, :new_id, :branch, :env do
   requires 'server restarted'
+  requires 'browser restarted'
 end
 
 dep 'server restarted', :template => 'task' do
@@ -13,5 +14,15 @@ dep 'server restarted', :template => 'task' do
       pid = output.scan(/^\S+\s+(\d+)\s+/).flatten.first
       log_shell "Sending SIGTERM to #{pid}", "kill -s TERM #{pid}"
     end
+  }
+end
+
+dep 'browser restarted', :template => 'task' do
+  run {
+    shell %q{osascript -e 'tell application "System Events"
+      tell process "Safari"
+      keystroke "r" using {command down}
+      end tell
+      end tell'}
   }
 end
